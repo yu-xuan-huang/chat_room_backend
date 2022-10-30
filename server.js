b367b72dffee826392f3378e52019116a8cad3bd
yu-xuan-huang <ga578007@gmail.com>
2022-10-30 17:15:55 +0800

    Add websocket

---
+++ server.js
@@ -0,0 +1,52 @@
+var ws = require("nodejs-websocket");
+var moment = require('moment');
+
+console.log("開始連接...")
+
+let users = [];
+
+// 向所有连接的客户端广播
+function boardcast(obj) {
+    server.connections.forEach(function (conn) {
+        conn.sendText(JSON.stringify(obj));
+    })
+}
+
+function getDate() {
+    return moment().format('YYYY-MM-DD HH:mm:ss')
+}
+
+var server = ws.createServer(function (conn) {
+    conn.on("text", function (obj) {
+        obj = JSON.parse(obj);
+        if (obj.type === 1) {
+            users.push({
+                nickname: obj.nickname,
+                uid: obj.uid
+            });
+            boardcast({
+                type: 1,
+                date: getDate(),
+                msg: obj.nickname + '加入聊天室',
+                users: users,
+                uid: obj.uid,
+                nickname: obj.nickname
+            });
+        } else {
+            boardcast({
+                type: 2,
+                date: getDate(),
+                msg: obj.msg,
+                uid: obj.uid,
+                nickname: obj.nickname
+            });
+        }
+    })
+    conn.on("close", function (code, reason) {
+        console.log("關閉連接")
+    });
+    conn.on("error", function (code, reason) {
+        console.log("啟動連接")
+    });
+}).listen(8001)
+console.log("WebSocket建立完成")
