e7839d87eb898ad0610b439d92b3e93aae6c7267
yu-xuan-huang <ga578007@gmail.com>
2022-10-31 22:30:17 +0800

    Add basic

--- server.js
+++ server.js
@@ -1,52 +1,65 @@
-var ws = require("nodejs-websocket");
+const express = require('express')
+const socketServer = require('ws').Server
 var moment = require('moment');
 
-console.log("開始連接...")
+//指定port
+const port = process.env.PORT || 3000;
 
-let users = [];
+//創建 express 的物件，並綁定及監聽 8081 port ，且設定開啟後在 console 中提示
+const server = express().listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
 
-// 向所有连接的客户端广播
-function boardcast(obj) {
-    server.connections.forEach(function (conn) {
-        conn.sendText(JSON.stringify(obj));
-    })
-}
+
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const socket = new socketServer({ server })
 
 function getDate() {
     return moment().format('YYYY-MM-DD HH:mm:ss')
 }
 
-var server = ws.createServer(function (conn) {
-    conn.on("text", function (obj) {
-        obj = JSON.parse(obj);
-        if (obj.type === 1) {
+//當 WebSocket 從外部連結時執行
+socket.on('connection', ws => {
+    //連結時執行此 console 提示
+    console.log('Client connected')
+    //對 message 設定監聽，接收從 Client 發送的訊息
+    ws.on('message', data => {
+        data = JSON.parse(data);
+        let newData;
+        let users = [];
+        if (data.type === 1) {
             users.push({
-                nickname: obj.nickname,
-                uid: obj.uid
+                nickname: data.nickname,
+                uid: data.uid
             });
-            boardcast({
+            newData = {
                 type: 1,
                 date: getDate(),
-                msg: obj.nickname + '加入聊天室',
+                msg: data.nickname + '加入聊天室',
                 users: users,
-                uid: obj.uid,
-                nickname: obj.nickname
-            });
-        } else {
-            boardcast({
+                uid: data.uid,
+                nickname: data.nickname,
+                ...data
+            }
+        }
+        else {
+            newData = {
                 type: 2,
                 date: getDate(),
-                msg: obj.msg,
-                uid: obj.uid,
-                nickname: obj.nickname
-            });
+                msg: data.msg,
+                uid: data.uid,
+                nickname: data.nickname
+            }
         }
+        let clients = socket.clients
+        //做迴圈，發送訊息至每個 client
+        clients.forEach(client => {
+            client.send(JSON.stringify(newData))
+        })
     })
-    conn.on("close", function (code, reason) {
-        console.log("關閉連接")
-    });
-    conn.on("error", function (code, reason) {
-        console.log("啟動連接")
-    });
-}).listen(8001)
-console.log("WebSocket建立完成")
+
+    //當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
